09c90210ef1e4cc2b0eb13c5d4fe61bb3857de62
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2024-06-30 23:45:05 -0300

    Exercicio03 concluido Cadastro de produto

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const express = require("express");
 
 const clientsRoutes = require("./routes/clients.routes");
+const productsRoutes = require("./routes/products.routes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/clients", clientsRoutes);
+app.use("/products", productsRoutes);
 
 const port = process.env.PORTA;
 
